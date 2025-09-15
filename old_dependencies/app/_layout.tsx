580c75314923516635399cfa51955b60c1e95787
iancapon <iancaponrusso@gmail.com>
2025-09-15 14:18:22 -0300

    reiniciado con sdk 54

--- old_dependencies/app/_layout.tsx
+++ old_dependencies/app/_layout.tsx
@@ -1,15 +1,17 @@
 import { Stack } from "expo-router";
 import { useEffect } from "react";
+import * as NavigationBar from "expo-navigation-bar";
 
 export default function RootLayout() {
-  
+  useEffect(() => {
+    NavigationBar.setVisibilityAsync("hidden");
+    //NavigationBar.setBehaviorAsync("overlay-swipe");
+  }, []);
   return (
     <Stack>
       <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
 
       <Stack.Screen name="(modals)" options={{headerShown: false}} />
-
-      <Stack.Screen name="Index"  />
     </Stack>
   );
 }
